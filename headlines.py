95d5f9aff472701877e01e0167209596f570cfaf
Dwaligon <odonnelljp01@gmail.com>
2013-03-07 02:48:28 -0500

    Update headlines.py
    Changed to google voice.  No longer writing to file.

--- headlines.py
+++ headlines.py
@@ -1,27 +1,26 @@
 #Jason O'Donnell and Justin Walrath   Text to Voice Robot
 #World New Headlines
-import re
 import feedparser
 import subprocess
 
-filename = 'headlines.txt'
-f = open(filename, 'w')
 #Parse feed
 d = feedparser.parse('http://www.npr.org/rss/rss.php?id=1001')
 
+#Need to keep string sizes down or get errors.
+headline0 = './speech.sh Heres the current world news:'
 headline1 = (d['items'][0]['title'])
 headline2 = (d['items'][1]['title'])
 headline3 = (d['items'][2]['title'])
 headline4 = (d['items'][3]['title'])
+headlineBreak = ":"
 
-#Write headlines to file
-f.write("Here is the current world news.:\n")
-f.write(headline1+":\n")
-f.write(headline2+":\n")
-f.write(headline3+":\n")
-f.write(headline4+":\n")
-f.close()
+head1 = "./speech.sh Heres the current world news: " + headline1 
+head2 = "./speech.sh " + headlineBreak + headline2 
+head3 = "./speech.sh " + headlineBreak + headline3 
+head4 = "./speech.sh " + headlineBreak + headline4 
 
-#Call linux command
-#subprocess.call('espeak -ven+m1 -f headlines.txt', shell=True)
-subprocess.call('festival --tts headlines.txt', shell=True)
+#Call linux command - Using Google Translate now.  Wayyyy better.
+subprocess.call(head1, shell=True)
+subprocess.call(head2, shell=True)
+subprocess.call(head3, shell=True)
+subprocess.call(head4, shell=True)
