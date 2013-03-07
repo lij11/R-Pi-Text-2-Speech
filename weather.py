13b65dbb30e39638d7064bc0ac2190a4a77b92e1
Dwaligon <odonnelljp01@gmail.com>
2013-03-07 02:49:30 -0500

    Update weather.py
    No longer writes to file, changed to google Translate.

--- weather.py
+++ weather.py
@@ -3,8 +3,6 @@ import re
 import feedparser
 import subprocess
 
-filename = 'voice.txt'
-f = open(filename, 'w')
 #re = regex.  Since the parse comes back as a bunch of nonsense, we filter
 #out words between ' '
 j = re.compile('\'(.*?)\'', re.IGNORECASE)
@@ -14,12 +12,13 @@ d = feedparser.parse('http://weather.yahooapis.com/forecastrss?w=2444988')
 condition = j.findall(str((d['items'][0]['yweather_condition'])))
 forecast =  j.findall(str((d['items'][0]['yweather_forecast'])))
 
-#Write the weather to the text file.
-f.write("Here is the weather for Mansfield Pennsylvania.\n")
-f.write("Currently: " + condition[3] + " and " + condition[7] + " degrees.\n")
-f.write("Forecast: " + forecast[3] + " and " + forecast[7] + " degrees.\n")
-f.close()
-#Call a linux console command.  Not sure what voice program I like more yet.
+weather = "./speech.sh Here is the weather for Mansfield Pennsylvania."
+current = "./speech.sh Currently: " + condition[3] + " and " + condition[7] + " degrees."
+forecast = "./speech.sh Forecast: " + forecast[3] + " and " + forecast[7] + " degrees."
 
-#subprocess.call('espeak -ven+m1 -f voice.txt', shell=True)
-subprocess.call('festival --tts voice.txt', shell=True)
+
+#Call a linux console command.  Using Google Translate
+subprocess.call(weather, shell=True)
+subprocess.call(current, shell=True)
+subprocess.call(forecast, shell=True)
+
