0b39766f0d969e9c01105fbc9b8d91bf2df78827
Justin Walrath <walrathjaw@gmail.com>
2013-03-07 23:54:50 -0500

    Tired but started the DB. Will confer with the team first before going further.

---
+++ dba/weatherpi.sql
@@ -0,0 +1,14 @@
+/*Database information for the raspberry pi bulletin board*/
+use pib;
+
+drop table if exists WEATHER;
+create table WEATHER(
+   WEATHER_ID int unsigned auto_increment,
+   WEATHER_DATE DATE not null,
+   WEATHER_TIMESTAMP datetime not null
+   WEATHER_CURRENT_TEMP int not null,
+   WEATHER_CURRENT_STATUS varchar(30) not null,
+   WEATHER_FORECAST_TEMP int not null,
+   WEATHER_FORECAST_STATUS varchar(30) not null,
+   primary key(WEATHER_ID)
+);
