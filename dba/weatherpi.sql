e6e808f546efbbd6803654898e3378abf1f95026
maximx1 <walrathjaw@gmail.com>
2013-03-07 23:55:54 -0500

    Forgot a comma

--- dba/weatherpi.sql
+++ dba/weatherpi.sql
@@ -5,7 +5,7 @@ drop table if exists WEATHER;
 create table WEATHER(
    WEATHER_ID int unsigned auto_increment,
    WEATHER_DATE DATE not null,
-   WEATHER_TIMESTAMP datetime not null
+   WEATHER_TIMESTAMP datetime not null,
    WEATHER_CURRENT_TEMP int not null,
    WEATHER_CURRENT_STATUS varchar(30) not null,
    WEATHER_FORECAST_TEMP int not null,
